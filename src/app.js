6140616182a9f4eb665e5d9bb47086dae82e8688
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-15 20:11:46 +0330

    GSAP

--- src/app.js
+++ src/app.js
@@ -5,33 +5,33 @@ document.addEventListener("DOMContentLoaded", (event) => {
     gsap.to('#BOX0', {
         rotation: 90,
         duration: 1,
-        x: 650,
-        y: 120,
+        x: -450,
         scrollTrigger: {
             trigger: "#BOX0",
-            toggleActions: "restart pause reverse restart"
+            toggleActions: "restart pause reverse restart",
+            scrub: true
         }
     })
     //! BOX1 
     gsap.to('#BOX1', {
         rotation: 180,
-        duration: 50,
-        x: 650,
+        duration: 1,
+        x: 450,
         scrollTrigger: {
             trigger: "#BOX1",
             toggleActions: "restart pause reverse restart",
-            scrub: true,
+            scrub: true
         }
     })
     //^ BOX2
     gsap.to("#BOX2", {
         rotation: 360,
-        duration: 50,
-        x: -650,
+        duration: 1,
+        x: -450,
         scrollTrigger: {
             trigger: "#BOX2",
             toggleActions: "restart pause reverse restart",
-            scrub: true,
+            scrub: true
         }
     })
     //& TEXT
