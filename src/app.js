ea44134733b7821ebbfd34344f4bd329a28d6497
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-16 03:59:39 +0330

    GSAP

--- src/app.js
+++ src/app.js
@@ -2,27 +2,27 @@
 //^ GSAP JAVASCRIPT FILE ================================================================================================================================
 document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger, TextPlugin, ScrollToPlugin)
-    gsap.to("#HEADER__CIRCLE1", {
+    gsap.to("#SQUARE__1", {
         x: -"60%",
         y: "-250%",
         duration: 2,
     })
-    gsap.to("#HEADER__CIRCLE2", {
+    gsap.to("#SQUARE__2", {
         x: "-50%",
         y: "-30%",
         duration: 2,
     })
-    gsap.to("#HEADER__CIRCLE3", {
+    gsap.to("#SQUARE__3", {
         x: "50%",
         y: "-180%",
         duration: 3,
     })
-    gsap.to("#HEADER__CIRCLE4", {
+    gsap.to("#SQUARE__4", {
         x: "70%",
         y: "-30%",
         duration: 1,
     })
-    gsap.to("#HEADER__CIRCLE5", {
+    gsap.to("#SQUARE__5", {
         x: "-20%",
         y: "-200%",
         duration: 3.5,
