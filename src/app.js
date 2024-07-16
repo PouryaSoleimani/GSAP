d28e20e87e3eb01f50fb9e316aa5b9684da2aedd
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-16 04:23:34 +0330

    GSAP

--- src/app.js
+++ src/app.js
@@ -2,58 +2,77 @@
 //^ GSAP JAVASCRIPT FILE ================================================================================================================================
 document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger, TextPlugin, ScrollToPlugin)
+    //^ SQUARES
     gsap.to("#SQUARE__1", {
-        x: -"60%",
-        y: "-250%",
-        duration: 2,
+        x: "110%",
+        y: "-170%",
+        duration: 1.5,
+        rotation : 140
     })
     gsap.to("#SQUARE__2", {
-        x: "-50%",
-        y: "-30%",
-        duration: 2,
+        x: "60%",
+        y: "-20%",
+        duration: 1.5,
+        rotation : 65
     })
     gsap.to("#SQUARE__3", {
         x: "50%",
-        y: "-180%",
-        duration: 3,
+        y: "-190%",
+        duration: 1.7,
+        rotation : 100
     })
     gsap.to("#SQUARE__4", {
         x: "70%",
-        y: "-30%",
-        duration: 1,
+        y: "-15%",
+        duration: 1.5,
+        rotation : 240
     })
     gsap.to("#SQUARE__5", {
         x: "-20%",
         y: "-200%",
-        duration: 3.5,
+        duration: 1.5,
+        rotation : 120
     })
-    //TODO BOX0 
+    gsap.to("#SQUARE__6", {
+        x: "-10%",
+        y: "-150%",
+        duration: 1.5,
+        rotation : 30
+    })
+    gsap.to("#SQUARE__7", {
+        x: "60%",
+        y: "-10%",
+        duration: 1.5,
+        rotation : 140
+    })
+    //^BOXES
+    //BOX__0
     gsap.to('#BOX0', {
         rotation: 360,
         duration: 1,
-        x: -850,
+        x: -1200,
         scrollTrigger: {
             trigger: "#BOX0",
             toggleActions: "restart pause reverse restart",
             scrub: true
         }
     })
-    //! BOX1 
+    //BOX__1
     gsap.to('#BOX1', {
         rotation: 360,
         duration: 1,
-        x: 950,
+        x: 1220,
         scrollTrigger: {
             trigger: "#BOX1",
             toggleActions: "restart pause reverse restart",
             scrub: true
         }
     })
-    //^ BOX2
+    //BOX__2
     gsap.to("#BOX2", {
         rotation: 360,
         duration: 1,
-        x: -940,
+        x: -1350,
         scrollTrigger: {
             trigger: "#BOX2",
             toggleActions: "restart pause reverse restart",
