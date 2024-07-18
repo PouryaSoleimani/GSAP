5ba635785fec3d1992365c0eb75036e85eeba7c9
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-18 22:57:33 +0330

    GSAP ESSENTIALS

--- src/app.js
+++ src/app.js
@@ -3,97 +3,27 @@ document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger, TextPlugin, ScrollToPlugin)
 
     //^ SQUARES
-    gsap.to("#SQUARE__1", {
-        x: "10%",
-        y: "-190%",
-        duration: 1.5,
-        rotation: 140
-    })
-    gsap.to("#SQUARE__2", {
-        x: "-30%",
-        y: "-10%",
-        duration: 1.5,
-        rotation: 65
-    })
-    gsap.to("#SQUARE__3", {
-        x: "20%",
-        y: "-240%",
-        duration: 1.7,
-        rotation: 100
-    })
-    gsap.to("#SQUARE__4", {
-        x: "90%",
-        y: "-15%",
-        duration: 1.5,
-        rotation: 240
-    })
-    gsap.to("#SQUARE__5", {
-        x: "20%",
-        y: "-200%",
-        duration: 1.5,
-        rotation: 120
-    })
-    gsap.to("#SQUARE__6", {
-        x: "40%",
-        y: "-150%",
-        duration: 1.5,
-        rotation: 30
-    })
+    gsap.to("#SQUARE__1", { x: "10%", y: "-190%", duration: 1.5, rotation: 140 })
+    gsap.to("#SQUARE__2", { x: "-30%", y: "-10%", duration: 1.5, rotation: 65 })
+    gsap.to("#SQUARE__3", { x: "20%", y: "-240%", duration: 1.7, rotation: 100 })
+    gsap.to("#SQUARE__4", { x: "90%", y: "-15%", duration: 1.5, rotation: 240 })
+    gsap.to("#SQUARE__5", { x: "20%", y: "-200%", duration: 1.5, rotation: 120 })
+    gsap.to("#SQUARE__6", { x: "40%", y: "-150%", duration: 1.5, rotation: 30 })
     //^BOXES
     //BOX__0
-    gsap.from('#BOX0', {
-        rotation: 60,
-        duration: 1,
-        x: "-210%",
-        scrollTrigger: {
-            trigger: "#BOX0",
-            toggleActions: "restart pause reverse restart",
-            scrub: true
-        }
-    })
+    gsap.from('#BOX0', { rotation: 60, duration: 1, x: "-210%", scrollTrigger: { trigger: "#BOX0", toggleActions: "restart pause reverse restart", scrub: true } })
     //BOX__1
-    gsap.from('#BOX1', {
-        rotation: 180,
-        duration: 1,
-        x: "200%",
-        scrollTrigger: {
-            trigger: "#BOX1",
-            toggleActions: "restart pause reverse restart",
-            scrub: true
-        }
-    })
+    gsap.from('#BOX1', { rotation: 180, duration: 1, x: "200%", scrollTrigger: { trigger: "#BOX1", toggleActions: "restart pause reverse restart", scrub: true } })
     //BOX__2
-    gsap.from("#BOX2", {
-        rotation: 120,
-        duration: 1,
-        x: "-240%",
-        scrollTrigger: {
-            trigger: "#BOX2",
-            toggleActions: "restart pause reverse restart",
-            scrub: true
-        }
-    })
+    gsap.from("#BOX2", { rotation: 120, duration: 1, x: "-240%", scrollTrigger: { trigger: "#BOX2", toggleActions: "restart pause reverse restart", scrub: true } })
     //& TEXT
-    gsap.to("#NEWTEXT", {
-        duration: 3,
-        text: "WELCOME TO GSAP",
-        ease: "none",
-        toggleActions: "restart pause reverse restart"
-    })
+    gsap.to("#NEWTEXT", { duration: 3, text: "WELCOME TO GSAP", ease: "none", toggleActions: "restart pause reverse restart" })
 
     // ^ LOGOS =================================================================================================================================
     //? SETTING ANIMATION TO ONE ELEMENT ( TRANSFORM X and Y ) || GSAP.TO & GSAP.FROM
     // gsap.to("#LOGO", {
     //     duration: 2,
     //     x: "350%",
-    //     backgroundColor: "#4b4a4a",
-    //     borderRadius: "20px",
-    //     border: "3px solid white",
-    //     ease: "back" // elastic , bounce 
-    // })
-    // gsap.to("#LOGO2", {
-    //     duration: 2,
-    //     x: "-350%",
     //     backgroundColor: "#4b4a4a",
     //     borderRadius: "20px",
     //     border: "3px solid white",
