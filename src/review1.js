fbbd27db2c7bdfc3b2b23b60777403339d671d59
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-08-16 15:52:54 +0330

    REFACTOR'

--- src/review1.js
+++ src/review1.js
@@ -1,23 +1,30 @@
-gsap.from("#BOX__REVIEW__1", {
-    duration: 3,
-    rotate: "-360",
-    x: "-100vw",
-    backgroundColor: "#FABB14",
-    ease: "back"
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(ScrollTrigger, TextPlugin, ScrollToPlugin)
+
+    gsap.from("#BOX__REVIEW__1", {
+        duration: 3,
+        rotate: "-360",
+        x: "-100vw",
+        backgroundColor: "#FABB14",
+        ease: "back"
+    })
+    gsap.from("#BOX__REVIEW__2", {
+        duration: 1.5,
+        scale: 0,
+        rotate: 720,
+        opacity: 0,
+        ease: "power1.inOut",
+        scrollTrigger: {
+            trigger: "#page2__REVIEW #BOX__REVIEW__2",
+            scroller: "body",
+            start: "top 150%",
+            end: "top 50%",
+            scrub: true,
+            markers: true
+        }
+    })
+
+
 })
-gsap.from("#BOX__REVIEW__2", {
-    duration: 1.5,
-    scale: 0,
-    rotate: 720,
-    opacity: 0,
-    ease: "power1.inOut",
-    scrollTrigger: {
-        trigger: "#page2__REVIEW #BOX__REVIEW__2",
-        scroller: "body",
-        start: "top 150%",
-        end: "top 50%",
-        scrub: true,
-        markers: true
-    }
-})
+
 
