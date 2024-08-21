a45aae1aa88031b5e3902479567232b872e48661
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-08-21 17:21:31 +0330

    REFACTOR

--- src/review1.js
+++ src/review1.js
@@ -39,9 +39,18 @@ gsap.from("#BOX__REVIEW__3", {
 })
 
 // ! CARDS
-// gsap.registerPlugin(ScrollTrigger)
-gsap.set(".card" ,{ position : "absolute"})
-gsap.to(".card" , {
-    yPercent : -100,
-    stagger : 0.5 , 
+gsap.registerPlugin(ScrollTrigger)
+gsap.set(".card", { position: "absolute" })
+
+gsap.to(".card", {
+    yPercent: -100,
+    stagger: 0.5,
+    scrollTrigger: {
+        trigger: ".cards__container",
+        markers: true,
+        start: "top top",
+        end: "=+2000px",
+        scrub: true,
+        pin: true,
+    }
 })
